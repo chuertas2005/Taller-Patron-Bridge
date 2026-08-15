86662641fbf119bc84b483d46333940a90c63d8d
cruzconk <santiaroca2007@gmail.com>
2026-08-14 22:12:57 -0500

    Delete FirmaLocal.java

--- FirmaLocal.java
+++
@@ -1,7 +0,0 @@
-/** Concrete signature provider: signs using a local server certificate. */
-public class FirmaLocal implements ProveedorFirma {
-    @Override
-    public void firmar(String contenido) {
-        System.out.println("[FirmaLocal] Firmando: " + contenido);
-    }
-}
