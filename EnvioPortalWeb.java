bc60774c1383b9a080e504fc502c94ad4d599cd6
cruzconk <santiaroca2007@gmail.com>
2026-08-14 22:10:09 -0500

    Delete EnvioPortalWeb.java

--- EnvioPortalWeb.java
+++
@@ -1,7 +0,0 @@
-/** Concrete channel: publishes the content on the self-service web portal. */
-public class EnvioPortalWeb implements CanalEnvio {
-    @Override
-    public void enviar(String contenido) {
-        System.out.println("[PortalWeb] Enviando: " + contenido);
-    }
-}
