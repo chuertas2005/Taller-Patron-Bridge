aed132034c806f62bb58fb9555507039253bbf2d
cruzconk <santiaroca2007@gmail.com>
2026-08-14 22:12:33 -0500

    Delete FirmaHSM.java

--- FirmaHSM.java
+++
@@ -1,7 +0,0 @@
-/** Concrete signature provider: signs using a hardware security module (HSM). */
-public class FirmaHSM implements ProveedorFirma {
-    @Override
-    public void firmar(String contenido) {
-        System.out.println("[FirmaHSM] Firmando: " + contenido);
-    }
-}
