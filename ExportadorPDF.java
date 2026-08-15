8b58da0029adbc43059310dae7f61ac6864f42aa
cruzconk <santiaroca2007@gmail.com>
2026-08-14 22:11:28 -0500

    Delete ExportadorPDF.java

--- ExportadorPDF.java
+++
@@ -1,7 +0,0 @@
-/** Concrete exporter: writes the content as PDF. */
-public class ExportadorPDF implements ExportadorFormato {
-    @Override
-    public void exportar(String contenido) {
-        System.out.println("[PDF] Exportando: " + contenido);
-    }
-}
