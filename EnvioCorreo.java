e87ce91ea78a2b9777d79f57a4f0f6eb3ec6d757
cruzconk <santiaroca2007@gmail.com>
2026-08-14 22:09:57 -0500

    Delete EnvioCorreo.java

--- EnvioCorreo.java
+++
@@ -1,7 +0,0 @@
-/** Concrete channel: sends the content by email. */
-public class EnvioCorreo implements CanalEnvio {
-    @Override
-    public void enviar(String contenido) {
-        System.out.println("[Correo] Enviando: " + contenido);
-    }
-}
