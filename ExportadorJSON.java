c78fd3f09d023614b5d54325e47ebaefe2725351
cruzconk <santiaroca2007@gmail.com>
2026-08-14 22:10:36 -0500

    Delete ExportadorJSON.java

--- ExportadorJSON.java
+++
@@ -1,7 +0,0 @@
-/** Concrete exporter: writes the content as JSON. */
-public class ExportadorJSON implements ExportadorFormato {
-    @Override
-    public void exportar(String contenido) {
-        System.out.println("[JSON] Exportando: " + contenido);
-    }
-}
