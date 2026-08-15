9c4f6f94abcafab483be504e93783896f12a3910
cruzconk <santiaroca2007@gmail.com>
2026-08-14 22:12:22 -0500

    Delete FirmaEnNube.java

--- FirmaEnNube.java
+++
@@ -1,7 +0,0 @@
-/** Concrete signature provider: signs using a cloud signing service. */
-public class FirmaEnNube implements ProveedorFirma {
-    @Override
-    public void firmar(String contenido) {
-        System.out.println("[FirmaEnNube] Firmando: " + contenido);
-    }
-}
