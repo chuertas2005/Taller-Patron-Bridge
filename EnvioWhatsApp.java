f1bb13bebe3a63ad01800bd7b3a90758e190ad7c
cruzconk <santiaroca2007@gmail.com>
2026-08-14 22:08:38 -0500

    Delete EnvioWhatsApp.java

--- EnvioWhatsApp.java
+++
@@ -1,7 +0,0 @@
-/** Concrete channel: sends the content by WhatsApp. */
-public class EnvioWhatsApp implements CanalEnvio {
-    @Override
-    public void enviar(String contenido) {
-        System.out.println("[WhatsApp] Enviando: " + contenido);
-    }
-}
